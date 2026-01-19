e978947c9daaf89fbc00daee159ecdb4c544de7b
liulu0713 <liu.l3@northeastern.edu>
2026-01-19 01:13:28 -0800

    debug while logical issue

--- homework-01-aloha-world-1/src/main/java/student/AlohaWorld.java
+++ homework-01-aloha-world-1/src/main/java/student/AlohaWorld.java
@@ -49,7 +49,7 @@ public final class AlohaWorld {
         Greeter greeter = new Greeter(name, locality);
         ConsoleView.printGreeting(greeter.greet());
 
-        while (!ConsoleView.checkRunAgain()) {
+        while (ConsoleView.checkRunAgain()) {
             locality = ConsoleView.getLocality();
             greeter.setLocality(locality);
             ConsoleView.printGreeting(greeter.greet());
